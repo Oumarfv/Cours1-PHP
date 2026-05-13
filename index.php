cf721749eb2801e37fd8eae5962b5faa97149e77
Oumarfv <m7senghor@gmail.com>
2026-05-13 11:52:13 +0000

    premier commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        body{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            overflow: hidden;
+        }
+    </style>
+
+</head>
+&copy;
+<body>
+    <form action="">
+        <div>
+            <label for="login">login</label>
+            <input id="login" type="text">
+        </div>
+
+        <div>
+            <label for="password">Password</label>
+            <input id="password" type="password">
+        </div>
+
+    </form>
+</body>
+</html>
---
+++ toto.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head> 
+<body>
+<center>
+<h2>FORMULAIRE DE CONNEXION</h2>
+                <div class="row justify-content-center"> 
+
+                    <div class="col-lg-8 col-xl-7">
+                        <form id="loginForm">
+                            <!-- tel-->
+                            <div class="form-floating mb-3">
+                                <label for="phone">Numéro de téléphone</label>  
+                                <input class="form-control" id="phone" type="tel" placeholder="(+221) 77-666-60-22" data-sb-validations="required" />
+                            </div> <br>
+
+                            <!-- mdp -->
+                            <div class="form-floating mb-3">
+                                <label for="password">Mot de passe</label>
+                               <input class="form-control" id="password" type="password" placeholder="******"  data-sb-validations="required"></input>
+                            </div>  
+                        </form>    
+                    </div>
+                </div>
+</center>
+                            
+</body>
+</html>
